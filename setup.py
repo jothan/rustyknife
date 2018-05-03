a40318e0bcf9995ca9880bcf656ebf209c4dac9f
Jonathan Bastien-Filiatrault <jonathan@zerospam.ca>
2018-05-03 14:38:43 -0400

    Intégration setuptools.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(name='rustyknife',
+      version='0.1.0',
+      rust_extensions=[RustExtension('rustyknife',
+                                     'Cargo.toml', binding=Binding.PyO3)],
+      zip_safe=False
+)
