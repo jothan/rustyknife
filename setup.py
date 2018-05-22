9d98fb513f1b66b831f9e98491145fdb801a32f7
Jonathan Bastien-Filiatrault <jonathan@zerospam.ca>
2018-05-22 07:20:04 -0400

    Version 0.1.1 starts.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(name='rustyknife',
-      version='0.1.0',
+      version='0.1.1',
       rust_extensions=[RustExtension('rustyknife',
                                      'Cargo.toml', binding=Binding.PyO3)],
       zip_safe=False
