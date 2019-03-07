eb62d40aa6add0b1846abcea07107b2ef9bd232d
Jonathan Bastien-Filiatrault <jonathan@zerospam.ca>
2019-03-07 10:21:27 -0500

    Update dependencies.

--- src/pymod.rs
+++ src/pymod.rs
@@ -11,9 +11,9 @@ use crate::util::{KResult, string_to_ascii};
 
 use memmap::Mmap;
 
-use pyo3::{self, Python, PyResult, PyModule, PyObject, PyBytes, PyTuple, IntoPyObject, ToPyObject, PyErr, PyDict};
-use pyo3::exc;
-use pyo3::py::modinit as pymodinit;
+use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyTuple, PyDict};
+use pyo3::types::exceptions as exc;
 
 impl ToPyObject for Address {
     fn to_object(&self, py: Python) -> PyObject {
@@ -130,24 +130,24 @@ fn fix_bare_cr(input: &[u8]) -> Vec<u8> {
         })
 }
 
-#[pymodinit(rustyknife)]
-fn init_module(py: Python, m: &PyModule) -> PyResult<()> {
+#[pymodule]
+fn rustyknife(_py: Python, m: &PyModule) -> PyResult<()> {
     /// from_(input)
     #[pyfn(m, "from_")]
     fn py_from(input: &PyBytes) -> PyResult<Vec<Address>> {
-        convert_result(from(input.data()), true)
+        convert_result(from(input.as_bytes()), true)
     }
 
     /// sender(input)
     #[pyfn(m, "sender")]
     fn py_sender(input: &PyBytes) -> PyResult<Address> {
-        convert_result(sender(input.data()), true)
+        convert_result(sender(input.as_bytes()), true)
     }
 
     /// reply_to(input)
     #[pyfn(m, "reply_to")]
     fn py_reply_to(input: &PyBytes) -> PyResult<Vec<Address>> {
-        convert_result(reply_to(input.data()), true)
+        convert_result(reply_to(input.as_bytes()), true)
     }
 
     /// header_section(input) -> ([headers...], end of headers position)
@@ -159,7 +159,7 @@ fn init_module(py: Python, m: &PyModule) -> PyResult<()> {
     /// :rtype: list of byte string tuples
     #[pyfn(m, "header_section")]
     fn py_header_section(py2: Python, input: &PyBytes) -> PyResult<PyObject> {
-        header_section_slice(py2, input.data())
+        header_section_slice(py2, input.as_bytes())
     }
 
     /// header_section_file(fname) -> ([headers...], end of headers position)
@@ -178,7 +178,7 @@ fn init_module(py: Python, m: &PyModule) -> PyResult<()> {
     /// xforward_params(input)
     #[pyfn(m, "xforward_params")]
     fn py_xforward_params(input: &PyBytes) -> PyResult<Vec<XforwardParam>> {
-        convert_result(xforward_params(input.data()), true)
+        convert_result(xforward_params(input.as_bytes()), true)
     }
 
     /// orcpt_address(input)
@@ -198,26 +198,26 @@ fn init_module(py: Python, m: &PyModule) -> PyResult<()> {
     ///
     /// :param input: Full SMTP MAIL command
     ///
-    ///  b"MAIL FROM:<user@example.org> BODY=7BIT"
+    ///  b'MAIL FROM:<user@example.org> BODY=7BIT'
     /// :type input: bytes
     /// :return: (address, [(param, param_value), ...])
     #[pyfn(m, "mail_command")]
     pub fn py_mail_command(input: &PyBytes) -> PyResult<(String, Vec<EsmtpParam>)>
     {
-        convert_result(mail_command(input.data()), true)
+        convert_result(mail_command(input.as_bytes()), true)
     }
 
     /// rcpt_command(input)
     ///
     /// :param input: Full SMTP RCPT command
     ///
-    ///  b"RCPT TO:<user@example.org> ORCPT=rfc822;user@example.org"
+    ///  b'RCPT TO:<user@example.org> ORCPT=rfc822;user@example.org'
     /// :type input: bytes
     /// :return: (address, [(param, param_value), ...])
     #[pyfn(m, "rcpt_command")]
     pub fn py_rcpt_command(input: &PyBytes) -> PyResult<(String, Vec<EsmtpParam>)>
     {
-        convert_result(rcpt_command(input.data()), true)
+        convert_result(rcpt_command(input.as_bytes()), true)
     }
 
     /// validate_address(address)
@@ -243,7 +243,7 @@ fn init_module(py: Python, m: &PyModule) -> PyResult<()> {
     /// :rtype: str
     #[pyfn(m, "unstructured")]
     fn py_unstructured(input: &PyBytes) -> PyResult<String> {
-        convert_result(unstructured(input.data()), true)
+        convert_result(unstructured(input.as_bytes()), true)
     }
 
     /// content_type(input, all=False)
@@ -252,13 +252,13 @@ fn init_module(py: Python, m: &PyModule) -> PyResult<()> {
         // FIXME: Figure out how to better handle a bare LF in
         // quoted-string without duplicating the rest of the RFC5322
         // syntax
-        convert_result(content_type(&fix_bare_cr(input.data())), all)
+        convert_result(content_type(&fix_bare_cr(input.as_bytes())), all)
     }
 
     /// content_disposition(input, all=False)
     #[pyfn(m, "content_disposition", input, all=false)]
     fn py_content_disposition(input: &PyBytes, all: bool) -> PyResult<(String, Vec<(String, String)>)> {
-        convert_result(content_disposition(&fix_bare_cr(input.data())), all)
+        convert_result(content_disposition(&fix_bare_cr(input.as_bytes())), all)
     }
 
     /// content_transfer_encoding(input, all=False)
@@ -266,7 +266,7 @@ fn init_module(py: Python, m: &PyModule) -> PyResult<()> {
     /// Parse a MIME Content-Transfer-Encoding header.
     ///
     /// Standard encodings such as 7bit, 8bit are accepted. Extended
-    /// encodings starting with a "x-" prefix are also accepted.
+    /// encodings starting with a 'x-' prefix are also accepted.
     ///
     /// :param input: Input string.
     /// :type input: bytes
@@ -275,7 +275,7 @@ fn init_module(py: Python, m: &PyModule) -> PyResult<()> {
     ///
     #[pyfn(m, "content_transfer_encoding", input, all=false)]
     fn py_content_transfer_encoding(input: &PyBytes, all: bool) -> PyResult<String> {
-        convert_result(content_transfer_encoding(&fix_bare_cr(input.data())), all)
+        convert_result(content_transfer_encoding(&fix_bare_cr(input.as_bytes())), all)
     }
 
     Ok(())
