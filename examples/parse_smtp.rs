592a4445df6628129abd396df54e388d143c2ab9
Jonathan Bastien-Filiatrault <jonathan@zerospam.ca>
2019-07-19 10:49:17 -0400

    Fix error output.

--- examples/parse_smtp.rs
+++ examples/parse_smtp.rs
@@ -15,7 +15,7 @@ fn main() -> Result<(), String> {
 
     let mut rem : &[u8] = &input;
     while !rem.is_empty() {
-        let (res, parsed) = command(rem).map_err(|e| "error")?;
+        let (res, parsed) = command(rem).map_err(|e| format!("{:?}", e))?;
 
         rem = res;
         println!("{:?}", parsed);
