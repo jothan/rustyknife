b3c26eb436f512f3cc26d3c481de8c5def9ee5fc
Jonathan Bastien-Filiatrault <jonathan@zerospam.ca>
2020-04-17 10:11:29 -0400

    Use complete header sections in tests.

--- src/tests/test_headersection.rs
+++ src/tests/test_headersection.rs
@@ -8,28 +8,28 @@ fn hs(i: &[u8]) -> Vec<HeaderField> {
 
 #[test]
 fn basic_line() {
-    let parsed = hs(b"X-Mozilla-Status: 0001\r\nX-Mozilla-Status2: 00800000\r\n");
+    let parsed = hs(b"X-Mozilla-Status: 0001\r\nX-Mozilla-Status2: 00800000\r\n\r\n");
     assert_eq!(parsed, [Ok((b"X-Mozilla-Status".as_ref(), b" 0001".as_ref())),
                         Ok((b"X-Mozilla-Status2".as_ref(), b" 00800000".as_ref()))]);
 }
 
 #[test]
 fn bad_nl() {
-    let parsed = hs(b"X-Mozilla-Status: 0001\r\nX-Mozilla-Status2: 00800000\nmore stuff\r\n".as_ref());
+    let parsed = hs(b"X-Mozilla-Status: 0001\r\nX-Mozilla-Status2: 00800000\nmore stuff\r\n\r\n".as_ref());
     assert_eq!(parsed, [Ok((b"X-Mozilla-Status".as_ref(), b" 0001".as_ref())),
                         Ok((b"X-Mozilla-Status2".as_ref(), b" 00800000\nmore stuff".as_ref()))]);
 }
 
 #[test]
 fn bad_cr() {
-    let parsed = hs(b"X-Mozilla-Status: 0001\r\nX-Mozilla-Status2: 00800000\rmore stuff\r\n".as_ref());
+    let parsed = hs(b"X-Mozilla-Status: 0001\r\nX-Mozilla-Status2: 00800000\rmore stuff\r\n\r\n".as_ref());
     assert_eq!(parsed, [Ok((b"X-Mozilla-Status".as_ref(), b" 0001".as_ref())),
                         Ok((b"X-Mozilla-Status2".as_ref(), b" 00800000\rmore stuff".as_ref()))]);
 }
 
 #[test]
 fn folded_header() {
-    let parsed = hs(b"X-Mozilla-Status: 0001\r\nContent-Type: multipart/alternative;\r\n  boundary=\"------------000500020107050007070009\r\nX-Mozilla-Status2: 00800000\r\n");
+    let parsed = hs(b"X-Mozilla-Status: 0001\r\nContent-Type: multipart/alternative;\r\n  boundary=\"------------000500020107050007070009\r\nX-Mozilla-Status2: 00800000\r\n\r\n");
     assert_eq!(parsed, [Ok((b"X-Mozilla-Status".as_ref(), b" 0001".as_ref())),
                         Ok((b"Content-Type".as_ref(), b" multipart/alternative;\r\n  boundary=\"------------000500020107050007070009".as_ref())),
                         Ok((b"X-Mozilla-Status2".as_ref(), b" 00800000".as_ref()))]);
@@ -37,7 +37,7 @@ fn folded_header() {
 
 #[test]
 fn big_garbage() {
-    let parsed = hs(b"X-Mozilla-Status: 0001\r\nbad header 00800000\r\nX-Mozilla-Keys: badly\nformated\nstuff is should \r w\nork#!@#$%\r^&*()_|\"}{P?><           \r\nanother bad header <4F34184B.7040006@example.com>\r\nDate: Thu, 09 Feb 2012 14:02:35 -0500\r\n".as_ref());
+    let parsed = hs(b"X-Mozilla-Status: 0001\r\nbad header 00800000\r\nX-Mozilla-Keys: badly\nformated\nstuff is should \r w\nork#!@#$%\r^&*()_|\"}{P?><           \r\nanother bad header <4F34184B.7040006@example.com>\r\nDate: Thu, 09 Feb 2012 14:02:35 -0500\r\n\r\n".as_ref());
     assert_eq!(parsed, [Ok((b"X-Mozilla-Status".as_ref(), b" 0001".as_ref())),
                         Err(b"bad header 00800000".as_ref()),
                         Ok((b"X-Mozilla-Keys".as_ref(), b" badly\nformated\nstuff is should \r w\nork#!@#$%\r^&*()_|\"}{P?><           ".as_ref())),
